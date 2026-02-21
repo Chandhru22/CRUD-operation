cb13c18de1eabfd579b3d2f5fcaf6def1dcde3aa
Chandhru22 <balachandhru314@gmail.com>
2026-02-21 13:41:56 +0530

    fix deployment settings

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)5&-p(c8p5iilg@ilt5wxbs@ywqtckipxi*sy1_hl=yo!thpez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['crud-operation-qeii.onrender.com']
 
 
 # Application definition
@@ -121,4 +122,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'items' / 'static',
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
